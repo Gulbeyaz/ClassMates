6f2b1e4febd57de262baeef35221a4813b27e519
Gulbeyaz <gulbeyazt@gmail.com>
2020-11-11 11:02:18 -0500

    added more group member's names

---
+++ src/additionalGroupMembers.java
@@ -0,0 +1,8 @@
+import java.util.Arrays;
+
+public class additionalGroupMembers {
+    public static void main(String[] args) {
+        String [] names= {"Nazar", "Shirin", "Sterline"};
+        System.out.println(Arrays.toString(names));
+    }
+}
