6773743a338d8c48d83620cc449c0c7f4cfd3cd0
Gulbeyaz <gulbeyazt@gmail.com>
2020-11-11 10:58:40 -0500

    added group member's names

--- src/namesForGroupMembers.java
+++ src/namesForGroupMembers.java
@@ -4,5 +4,6 @@ public class namesForGroupMembers {
     public static void main(String[] args) {
         String [] names= {"Marwan", "Alex", "Bayana", "Akbar", "Cem"};
         System.out.println(Arrays.toString(names));
+
     }
 }
