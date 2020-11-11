c613d20f48dceecd172f80ff7c53fd554a04c0e6
Gulbeyaz <gulbeyazt@gmail.com>
2020-11-11 10:53:24 -0500

    added class member's names

--- src/namesForClassMembers.java
+++ src/namesForClassMembers.java
@@ -1,6 +1,6 @@
 import java.util.Arrays;
 
-public class namesForClassMates {
+public class namesForClassMembers {
     public static void main(String[] args) {
         String [] names={"Kubra", "Ayse", "Yasin", "Inna", "Nejdet", "Asia"};
         System.out.println(Arrays.toString(names));
---
+++ src/namesForGroupMembers.java
@@ -0,0 +1,8 @@
+import java.util.Arrays;
+
+public class namesForGroupMembers {
+    public static void main(String[] args) {
+        String [] names= {"Marwan", "Alex", "Bayana", "Akbar", "Cem"};
+        System.out.println(Arrays.toString(names));
+    }
+}
