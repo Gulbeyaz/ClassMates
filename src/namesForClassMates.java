53e6b8f3accbf9b33c735d8ed51eb1c34adbdfcd
Gulbeyaz <gulbeyazt@gmail.com>
2020-11-11 10:49:38 -0500

    printed class member's names

--- src/namesForClassMates.java
+++ src/namesForClassMates.java
@@ -1,5 +1,9 @@
+import java.util.Arrays;
+
 public class namesForClassMates {
     public static void main(String[] args) {
         String [] names={"Kubra", "Ayse", "Yasin", "Inna", "Nejdet", "Asia"};
+        System.out.println(Arrays.toString(names));
+
     }
 }
